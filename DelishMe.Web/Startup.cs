3f1be73a6e62e38030a592ff28eb89b0bebfd91a
Stoianov Artiom <stoyanov.201515@gmail.com>
2024-04-16 11:02:08 +0300

    Authorization

---
+++ DelishMe.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DelishMe.Web.Startup))]
+namespace DelishMe.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
