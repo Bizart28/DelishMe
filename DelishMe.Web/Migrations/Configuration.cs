90e2f653e3147c648629bd925e3380bdfd94b724
Stoianov Artiom <stoyanov.201515@gmail.com>
2024-04-18 15:50:57 +0300

    migrations enabled

--- DelishMe.Web/Migrations/Configuration.cs
+++ DelishMe.Web/Migrations/Configuration.cs
@@ -5,14 +5,14 @@
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<DelishMe.Web.Models.MyDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<DelishMe.Web.Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(DelishMe.Web.Models.MyDbContext context)
+        protected override void Seed(DelishMe.Web.Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
