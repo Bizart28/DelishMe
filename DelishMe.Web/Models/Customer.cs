e65bfdb2957fd86e98b68b6a489aa6c91616f1e1
Stoianov Artiom <stoyanov.201515@gmail.com>
2024-04-24 17:14:29 +0300

    membership type added to customer

--- DelishMe.Web/Models/Customer.cs
+++ DelishMe.Web/Models/Customer.cs
@@ -9,5 +9,8 @@ namespace DelishMe.Web.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }   
+        public bool IsSubcribedToNewsLetter { get; set; }
+        public  MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; }
     }
 }
