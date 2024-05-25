90c4b30f885a1bcb103c564322598bdaac32d70b
Stoianov Artiom <stoyanov.201515@gmail.com>
2024-05-25 17:10:00 +0300

    Customer DTO

---
+++ DelishMe.Web/Dtos/CustomerDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using DelishMe.Web.Models;
+
+namespace DelishMe.Web.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter customer's name.")]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNewsLetter { get; set; }
+        [ForeignKey("MembershipType")]
+        public byte MembershipTypeId { get; set; }
+        [_18YearsValidation]
+        public DateTime? Birthdate { get; set; }
+    }
+}
